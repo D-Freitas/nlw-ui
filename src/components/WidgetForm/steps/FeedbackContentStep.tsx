59ab372a1309d6df1df90f8ffb85ec133446a9ae
d-freitas <davi.israel01@gmail.com>
2022-05-03 12:08:30 -0300

    feat: add back button to FeedbackContentStep

--- src/components/WidgetForm/steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/steps/FeedbackContentStep.tsx
@@ -4,16 +4,25 @@ import { CloseButton } from '../../CloseButton'
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType
+  onFeedbackRestartRequested: () => void
 }
 
 export function FeedbackContentStep({
   feedbackType,
+  onFeedbackRestartRequested
 }: FeedbackContentStepProps) {
   const feedbackTypeInfo = feedbackTypes[feedbackType]
 
   return (
     <>
       <header>
+        <button
+          className='top-5 left-5 absolute text-zinc-400 hover:text-zinc-100'
+          onClick={onFeedbackRestartRequested}
+        >
+          <ArrowLeft weight='bold' className='w-4 h-4' />
+        </button>
+
         <span className='text-xl leading-6 flex items-center gap-2'>
           <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className='w-6 h-6' />
           {feedbackTypeInfo.title}
@@ -21,10 +30,6 @@ export function FeedbackContentStep({
 
         <CloseButton />
       </header>
-
-      <div className='flex py-8 gap-2 w-full'>
-
-      </div>
     </>
   )
 }
